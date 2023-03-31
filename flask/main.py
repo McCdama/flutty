acacdb519584b11911e583fa20e5b182c26c9fc3
Mohed Alrahbi <mccdama@gmail.com>
2023-03-31 03:31:57 +0200

    Get rid of Celery

--- flask/main.py
+++ flask/main.py
@@ -3,22 +3,17 @@ import yt_dlp
 
 app = Flask(__name__)
 
-def download_video(url, filename):
-    ydl_opts = {
-        'outtmpl': filename,
-        'external_downloader': 'aria2c',
-        'external_downloader_args': ['-n', '16']
-    }
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    ydl_opts = {'outtmpl': 'video.mp4'}
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-@app.route('/download', methods=['POST'])
-def start_download():
-    url = request.form['url']
-    video_title = yt_dlp.YoutubeDL().extract_info(url, download=False).get('title', 'video')
-    filename = f"{video_title}.mp4"
-    download_video.delay(url, filename)
-    return f"Downloading {video_title}..."
+    return send_file('video.mp4', as_attachment=True)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, use_reloader=True, threaded=True, debug=True)
+
+# sample url: https://www.youtube.com/watch?v=M3TZx-2G8eY&ab_channel=MadMuscles

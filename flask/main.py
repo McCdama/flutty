7af255ffd2b02e729101e1a0770bd9bf3e15ed48
Mohed Alrahbi <mccdama@gmail.com>
2023-03-31 03:45:24 +0200

    Flask gitignore File & Logger

--- flask/main.py
+++ flask/main.py
@@ -2,11 +2,30 @@ from flask import Flask, request, send_file
 import yt_dlp
 
 app = Flask(__name__)
+class Logger:
+    def debug(self, msg):
+        #if msg.startswith('[error] '):
+        #    self.error(msg)
+
+        def error(self, msg):
+            print(msg)
+
+def hook(d):
+    if d['status'] == 'downloading':
+        print('Downloading...')
+
+    if d['status'] == 'finished':
+        print('Done downloading, now post-processing ...')
 
 @app.route('/download', methods=['POST'])
 def download():
+    print("request recieved...")
     url = request.form['url']
-    ydl_opts = {'outtmpl': 'video.mp4'}
+    ydl_opts = {
+        'outtmpl': 'video.mp4',
+        # 'logger': Logger(),
+        # 'progress_hooks': [hook],
+        }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
